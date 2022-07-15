fe394ea121c1957b391a039cbe17ccbbb7d218d8
up4k <panov73panov@gmail.com>
2022-07-15 12:53:48 +0400

    Добавил комментарий к функции arr.map, тестовый коммит

--- src/App.js
+++ src/App.js
@@ -3,6 +3,29 @@ import Header from "./components/Header";
 import Search from "./components/Search";
 import Drawer from "./components/Drawer";
 
+const arr = [
+  {
+    name: "Мужские Кроссовки Nike Blazer Mid Suede",
+    price: "12999 руб.",
+  },
+  {
+    name: "Кроссовки adidas",
+    price: "18999 руб.",
+  },
+  {
+    name: "Кроссовки Puma X Aka Boku Future Rider",
+    price: "20999 руб.",
+  },
+  {
+    name: "Мужские Кроссовки Nike Kyrie Flytrap IV",
+    price: "28999 руб.",
+  },
+  {
+    name: "Мужские Кроссовки Nike LeBron XVIII",
+    price: "38999 руб.",
+  },
+];
+
 function App() {
   return (
     <div className="wrapper clear">
@@ -10,48 +33,14 @@ function App() {
       <Header />
       <div className="content p-40">
         <Search />
-        <div className="d-flex flex-wrap">
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
+        <div className="d-flex justify-center flex-wrap">
+          {arr.map(
+            (
+              val // При помощи метода arr.map из массива вытаскиваются объекты и заменяются на единицу
+            ) => (
+              <b>1</b>
+            )
+          )}
           <Card />
           <Card />
           <Card />
