d5e6bdd7a7cd9a7937f1191359da8903bde2951d
up4k <panov73panov@gmail.com>
2022-07-18 15:46:11 +0400

    пример с useState в файле SomethingToTest

--- src/components/SomethingToTest.js
+++ src/components/SomethingToTest.js
@@ -1,5 +1,24 @@
 import React from "react";
 
 export default function SomethingToTest() {
-  return <div></div>;
+  const [count, setCount] = React.useState(0);
+  const plus = () => {
+    setCount(count + 1);
+  };
+  const minus = () => {
+    setCount(count - 1);
+  };
+
+  return (
+    <div className="wrapper clear">
+      <center>
+        {" "}
+        <h1>{count}</h1>
+        <div>
+          <button onClick={plus}>+</button>
+          <button onClick={minus}>-</button>
+        </div>
+      </center>
+    </div>
+  );
 }
