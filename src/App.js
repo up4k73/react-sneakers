2e9b77cd5aa3804ffa494146bdb37556e5c1f77c
up4k <panov73panov@gmail.com>
2022-07-20 10:01:42 +0400

    fetch запрос в App

--- src/App.js
+++ src/App.js
@@ -10,30 +10,36 @@ import Overlay from "./components/Overlay/Overlay";
 function App() {
   const [items, setItems] = React.useState([]);
   const [cartOpened, setcartOpened] = React.useState(false);
-  const [isCartOpened, setisCartOpened] = React.useState(false); //закрытие|открытие Overlay, нужно будет подумать, как реализовать по-другому. Повторяющиеся действия.
-
+  //const [isCartOpened, setisCartOpened] = React.useState(false); //закрытие|открытие Overlay, нужно будет подумать, как реализовать по-другому. Повторяющиеся действия.
+  fetch("https://62d68bb849c87ff2af269c1b.mockapi.io/items")
+    .then((res) => {
+      return res.json();
+    })
+    .then((res) => {
+      setItems(res);
+    });
   return (
     <div className="wrapper clear">
       {cartOpened ? ( //Если в useState true, тогда рендерить Drawer, если false, тогда ничего не рендерится
         <Drawer //В Drawer в качестве пропсов передается функция, которая изменяет значение cartOpened
           onClose={() => {
             setcartOpened(false);
-            setisCartOpened(false);
+            //setisCartOpened(false);
           }}
         />
       ) : null}
-      {isCartOpened ? (
+      {cartOpened ? (
         <Overlay
           onOverlay={() => {
             setcartOpened(false);
-            setisCartOpened(false);
+            //setisCartOpened(false);
           }}
         />
       ) : null}
       <Header
         onClickCart={() => {
           setcartOpened(true);
-          setisCartOpened(true);
+          //setisCartOpened(true);
         }}
       />
       <div className="content p-40">
