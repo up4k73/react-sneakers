ad6b9929feac1ed72a377a04b7d84b9fef01f489
up4k <panov73panov@gmail.com>
2022-07-27 15:51:21 +0400

    Почистил лишние импорты в index.js

--- src/index.js
+++ src/index.js
@@ -1,18 +1,15 @@
 import React from "react";
 import {
   BrowserRouter,
-  Routes,
-  Route,
-  Router,
+
 } from "react-router-dom";
 import ReactDOM from "react-dom/client";
 import "./index.scss";
 import "macro-css";
 import App from "./App";
-import Favorites from "./components/Pages/Favorites";
 
-import CommentSection from "./components/CommentSection/CommentSection";
-import Header from "./components/Header";
+
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
